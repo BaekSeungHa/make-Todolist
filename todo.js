9c9488644692d173ffda8c0f8db5bbcfda50e01b
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-04-28 22:18:30 +0900

    study
    test

--- todo.js
+++ todo.js
@@ -1,32 +1,45 @@
 const input = document.querySelector("input");
 const addBtn = document.querySelector("#addBtn");
 const ul = document.querySelector("ul");
-
+let todos = JSON.parse(localStorage.getItem("todoList")) || [];
 
 addBtn.addEventListener("click", addTodo);
+
 function addTodo() {
     const todo = input.value;
-    if(todo == ""){
-        return 0;
-    }
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    const delBtn = document.createElement("button");
-    delBtn.innerText = "❌";
-    delBtn.addEventListener('click', deleteTodo);
-    span.innerText = todo; //글씨 넣는거 todo 부터 아래로 시작
-    li.appendChild(span);
-    li.appendChild(delBtn);
+    if (todo) {
+        paintTodo(todo);
+       todos.push(todo);
+       console.log(todos);
+       input.value = "";
+      //   saveTodo();
+  }
+}
 
-    ul.appendChild(li);
-
-    input.value = "";
-    }
-
-
-function deleteTodo(event) {
-    console.log(event.target);
+function deleteTodo(event, delTodo) {
     const btn = event.target;
     const li = btn.parentNode;
     ul.removeChild(li);
+  
+    todos = todos.filter((todo) => todo != delTodo);
+    console.log(todos);
+  
+    // saveTodo();
+}
+
+function paintTodo(todo) {
+    const li = document.createElement('li')
+    const span = document.createElement("span");
+    const delBtn = document.createElement("button");
+
+    span.innerText = todo;
+    delBtn.innerText = "❌";
+
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    delBtn.addEventListener("click", (event) => {
+    deleteTodo(event, todo);
+    });
+
+    ul.appendChild(li);
 }
